56278dc13a54c8b03edcadeea904d6ac53942bc1
mirandadayadkins <mirandaday16@gmail.com>
2021-02-27 14:10:54 -0500

    added base site styles and classes for home page and navigation bar

---
+++ src/components/base-app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BaseApp extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+
+            </div>
+        )
+    }
+}
+
+export default BaseApp
