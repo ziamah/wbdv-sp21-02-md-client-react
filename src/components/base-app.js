5186e8c58065ca75a5325990695614039ae98bd6
mirandadayadkins <mirandaday16@gmail.com>
2021-02-27 19:36:25 -0500

    added route urls

--- src/components/base-app.js
+++ src/components/base-app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Route, Link} from "react-router-dom";
 
 class BaseApp extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-
+                <Route path="/home"></Route>
+                <Route path="/profile"></Route>
+                {/*<Route path="/results"></Route>*/}
+                {/*<Route path="details"></Route>*/}
+                <Route path="/login"></Route>
+                <Route path="/register"></Route>
             </div>
         )
     }
