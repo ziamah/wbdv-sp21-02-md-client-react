c82a95c22023ec676bae80ac71c26518aa512608
mirandadayadkins <mirandaday16@gmail.com>
2021-03-15 15:54:39 -0400

    adding navlinks to navbar

---
+++ src/components/navigation-bar/navigation-links.js
@@ -0,0 +1,13 @@
+const NavigationLinks = () => {
+    return (
+        <>
+            <button className="btn wbdv-navbar-btn">home</button>
+            <button className="btn wbdv-navbar-btn">profile</button>
+            <button className="btn wbdv-navbar-btn">
+                <i className="fas fa-plus-circle"></i>
+            </button>
+        </>
+    )
+}
+
+export default NavigationLinks
