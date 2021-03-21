29349572b96d5ac30eae197914d92e2eddd66f4d
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-03-21 10:09:53 -0400

    adding components for profile sections

--- src/components/profile/user-list.js
+++ src/components/profile/user-list.js
@@ -2,14 +2,18 @@ import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {useParams, Link} from "react-router-dom";
 
-const UserList = ({users = []}) => {
+const UserList = ({users = [], heading = "My Followers"}) => {
 
     return (
+        <>
+        <h1> {heading} </h1>
+        <br/>
         <ul>
         {
-            users.map(user => <li>user</li>)
+            users.map(user => <li>{user}</li>)
         }
         </ul>
+        </>
 
 
     )
