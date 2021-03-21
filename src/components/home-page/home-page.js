26fd6ca9821ad1c4c01b8730323bf530efea613c
mirandadayadkins <mirandaday16@gmail.com>
2021-03-21 12:08:45 -0400

    added sign up/in widget to home page

--- src/components/home-page/home-page.js
+++ src/components/home-page/home-page.js
@@ -1,12 +1,15 @@
 import React from "react";
 import FeaturedRecipeCard from "./featured-recipe-card";
 import FavoritesWidget from "./favorites-widget";
+import SignUpWidget from "./sign-up-widget";
 
 class HomePage extends React.Component {
     render() {
         return (
             <div className="row">
                 <div className="col-4 d-none d-lg-block">
+                    {/*TODO: Should choose one of these two widgets depending on whether user is signed in*/}
+                    <SignUpWidget/>
                     <FavoritesWidget/>
                 </div>
                 <div className="col-8">
---
+++ src/components/home-page/sign-up-widget.js
@@ -0,0 +1,36 @@
+import {Link} from "react-router-dom";
+
+const SignUpWidget = () => {
+    return (
+        <div className="wbdv-widget-container">
+            <div className="wbdv-contrast-header">
+                <h3 className="wbdv-center-in-div">Not signed in?</h3>
+            </div>
+            <div className="wbdv-widget-interior">
+                <div className="row wbdv-center-in-div">
+                    {/*TODO: set Link address to sign in page*/}
+                    <Link to={"#"}>
+                        <button className="btn wbdv-affirmative-btn">
+                            SIGN IN
+                        </button>
+                    </Link>
+                </div>
+                <div className="wbdv-center-in-div wbdv-widget-interior">
+                    or
+                </div>
+                <div className="row wbdv-center-in-div">
+                    {/*TODO: set Link address to sign up page*/}
+                    <Link to={"/details"}>
+                        <button className="btn wbdv-affirmative-btn">
+                            CREATE AN ACCOUNT
+                        </button>
+                    </Link>
+                </div>
+                <br/>
+            </div>
+
+        </div>
+    )
+}
+
+export default SignUpWidget
