3ca3ff46503a62840b8c836d040f3d1775f0d359
mirandadayadkins <mirandaday16@gmail.com>
2021-03-22 11:47:12 -0400

    fixed screen size responsiveness for new recipe page

--- src/components/new-recipe-page/new-recipe-page.js
+++ src/components/new-recipe-page/new-recipe-page.js
@@ -10,7 +10,7 @@ const NewRecipe = () => {
             </h1>
             <hr/>
             <div className="row">
-                <div className="col-4">
+                <div className="col-12 col-lg-4 wbdv-padded-image">
                     <div className="row">
                         <img className="d-block w-100 wbdv-padded-img"
                              src="https://soilhealthinstitute.org/wp-content/uploads/2016/11/thumbnail-default.jpg"
@@ -21,25 +21,27 @@ const NewRecipe = () => {
                             Click to add a photo
                         </button>
                     </div>
+                    <br/>
                 </div>
                 <div className="col-8">
                     {/*    TODO: Connect form to server*/}
 
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-lg-2">
                             <label htmlFor="title-field" className="wbdv-label">Recipe Title: </label>
                         </div>
-                        <input id="title-field" className="col-8" placeholder="Abuela's Famous Birria Tacos"/>
+                        <input id="title-field" className="col-lg-8 col-12" placeholder="Abuela's Famous Birria Tacos"/>
                     </p>
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-lg-2">
                             <label htmlFor="summary-field" className="wbdv-label">Summary: </label>
                         </div>
-                        <textarea id="summary-field" className="col-8"
-                                  placeholder="A few sentences describing your recipe"/>
+                        <textarea id="summary-field" className="col-lg-8 col-12"
+                                  placeholder="A few sentences describing your recipe"
+                                  rows="3"/>
                     </p>
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-lg-2">
                             <label htmlFor="diet-options" className="wbdv-label">Diet Tags: </label>
                         </div>
                         {/*TODO: Allow selector buttons to add diet tags*/}
@@ -50,7 +52,7 @@ const NewRecipe = () => {
                         </div>
                     </p>
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-lg-2">
                             <label htmlFor="ingredient-options" className="wbdv-label">Ingredient Tags: </label>
                         </div>
                         {/*TODO: Allow selector buttons to add diet tags*/}
@@ -61,17 +63,17 @@ const NewRecipe = () => {
                         </div>
                     </p>
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-xl-2">
                             <label htmlFor="ingredients-field" className="wbdv-label">Ingredient List: </label>
                         </div>
-                        <textarea id="ingredients-field" className="col-8" rows="5"
+                        <textarea id="ingredients-field" className="col-xl-8 col-12" rows="5"
                                   placeholder={`1. 1 cup of water\n2. 2 large eggs\n3. ...`}/>
                     </p>
                     <p className="row">
-                        <div className="col-2">
+                        <div className="col-12 col-xl-2">
                             <label htmlFor="instructions-field" className="wbdv-label">Instructions: </label>
                         </div>
-                        <textarea id="instructions-field" className="col-8" rows="7"
+                        <textarea id="instructions-field" className="col-xl-8 col-12" rows="7"
                                   placeholder="Write the recipe steps here"/>
                     </p>
                     <div className="row wbdv-center-in-div">
