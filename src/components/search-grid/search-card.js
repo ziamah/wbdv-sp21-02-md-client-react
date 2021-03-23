b0ff270f0867a515ab412cdd55ae07ba5fca0118
ziam <ziam@husky.neu.edu>
2021-03-23 15:09:59 -0400

    Search page + cards

---
+++ src/components/search-grid/search-card.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+import {useHistory, useParams} from 'react-router-dom'
+import recipeService from '../../services/recipe-service'
+
+const SearchCard = () => {
+    const {imdbID} = useParams()
+    const history = useHistory()
+    const [movie, setMovie] = useState({})
+    useEffect(() => {
+        findMovieByImdbID()
+    }, [])
+    const findMovieByImdbID = () => {
+        recipeService.findMovieByImdbID(imdbID)
+            .then((data) => {
+                setMovie(data)
+            })
+    }
+    return(
+
+    <div className="card mb-3" style="max-width: 540px;">
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src="..." alt="..."/>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a little bit
+                        longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins
+                        ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default SearchCard
+
+
---
+++ src/components/search-grid/search-grid.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react'
+import {Link, useParams, useHistory} from "react-router-dom";
+import recipeService from '../../services/recipe-service'
+//import SearchCard from "./search-card";
+//TODO: import recipe service after it's made
+
+const SearchGrid = () => {
+    const history = useHistory()
+    const {title} = useParams()
+    const [searchTitle, setSearchTitle] = useState(title)
+    const [results, setResults] = useState({Search: []})
+    useEffect(() => {
+        setSearchTitle(title)
+        findMoviesByTitle(title)
+    }, [])
+    const findMoviesByTitle = (title) => {
+        history.push(title)
+        recipeService.findMoviesByTitle(title)
+            .then((results) => {
+                setResults(results)
+            })
+    }
+    return(
+        <div>
+            <h2>Search Results</h2>
+            <div className="row">
+                <div className="col-9">
+                    <input value={searchTitle}
+                           onChange={(event) => {
+                               setSearchTitle(event.target.value)
+                           }}
+                           className="form-control"/>
+                </div>
+                <div className="col-3">
+                    <button
+                        onClick={() => {
+                            findMoviesByTitle(searchTitle)
+                        }}
+                        className="btn btn-primary btn-block">
+                        Search
+                    </button>
+                </div>
+            </div>
+            <br/>
+            <ul className="list-group">
+                {
+                    results && results.Search && results.Search.map((movie) => {
+                        return(
+                            <li className="list-group-item">
+                                <Link to={`/search-card/${movie.imdbID}`}>
+                                    {movie.Title}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default SearchGrid
