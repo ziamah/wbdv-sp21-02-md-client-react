cc9361c0a346dd851f81d5e805130a1134ec9040
mirandadayadkins <mirandaday16@gmail.com>
2021-04-04 17:30:11 -0400

    rebase cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-<<<<<<< HEAD
 import {BrowserRouter} from "react-router-dom";
 import BaseApp from "./components/base-app";
 
@@ -9,24 +8,7 @@ function App() {
       <BrowserRouter>
         <BaseApp/>
       </BrowserRouter>
-=======
-import {BrowserRouter, Route} from "react-router-dom";
-import Profile from './components/profile/profile'
 
-function App() {
-  return (
-
-    <BrowserRouter>
-        <Route path={"/profile/user/:userId"} exact={true} render={() => <Profile/>}>
-    {/*<div className="">*/}
-
-            {/*<Profile/>*/}
-
-    {/*</div>*/}
-        </Route>
-    </BrowserRouter>
-
->>>>>>> 8a848e2903ce8c9a907fc894a3f16a13c1f5d30f
   );
 }
 
