1c96cfc1dc7e6192694535764c1baed90c3b2f32
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-04-03 21:48:17 -0400

    some contents are coming from database..can update user name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
 import Profile from './components/profile/profile'
 
 function App() {
   return (
-    <div className="">
-      <Profile/>
-    </div>
+
+    <BrowserRouter>
+        <Route path={"/profile/user/:userId"} exact={true} render={() => <Profile/>}>
+    {/*<div className="">*/}
+
+            {/*<Profile/>*/}
+
+    {/*</div>*/}
+        </Route>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react';
-import {BrowserRouter,Link,Route} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter,Link,Route, useParams} from 'react-router-dom';
 import PrivateData from './private-data';
 import './profile.css'
 import UserList from './user-list'
 import RecipeList from './recipe-list'
 import NavigationBar from './navigation-bar'
+import userService from "../profile-services/user-service";
 
-const Profile = ({userid="superman", following="ab,cd,ef", followers="ab,cd,ef",
+const Profile = ({following="ab,cd,ef", followers="ab,cd,ef",
                  likes="recipe1,recipe2", recipes="myrecipe1,myrecipe2"}) => {
 
     const [privatemode, setPrivateMode] = useState(true);
     const [editing, setEditing] = useState(false);
+    const [userName, setUserName] = useState(" ")
+    const [profileImage, setProfileImage] = useState(" ")
+    const [userBio, setUserBio] = useState(" ")
+    const [user, setUser] = useState()
+    const {userId} = useParams();
+    console.log({userId});
+
+    useEffect(() => {
+        userService.findUserById(userId)
+                    .then(user => {
+                                   setUserName(user.userName);
+                                   setProfileImage(user.userPicUrl);
+                                   setUserBio(user.userBio);
+                                   setUser(user);
+                                   })
+
+        }, [userId])
+
+    console.log(userName)
+    console.log(profileImage)
+    console.log(userBio)
 
 
     return(
-    <BrowserRouter>
+    <div>
+    {/*<Route path="/profile/user/:userId" exact={true}>*/}
     <div>
         <NavigationBar/>
     </div>
@@ -28,15 +51,15 @@ const Profile = ({userid="superman", following="ab,cd,ef", followers="ab,cd,ef",
             </div>
             <div className="card col-lg-8 add-padding-pic profile-card2 background-liked profile-card3">
 
-                    <img src="https://i.ibb.co/T8hppc1/anna-pelzer-IGf-IGP5-ONV0-unsplash.jpg"
+                    <img src={profileImage}
                         className="card-image-top profile-image-size rounded-circle" alt="..."/>
 
 
                 <div class="card-body profile-image-size col-6">
 
-                    <div><h3 className="card-title color-green profile-bold">MManzur Morshed</h3></div>
+                    <div><h3 className="card-title color-green profile-bold">{userName}</h3></div>
                     <div className="color-black review-numbers-font-size">10 reviews!</div>
-                    <div className="card-text color-brown">I am a food lover. Like to explore new recipes!</div>
+                    <div className="card-text color-brown">{userBio}</div>
 
                 </div>
 
@@ -105,7 +128,12 @@ const Profile = ({userid="superman", following="ab,cd,ef", followers="ab,cd,ef",
                     </div>
 
                     <div className="h3 add-padding col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                         <PrivateData/>
+                         <PrivateData userName = {userName}
+                                      setUserName = {setUserName}
+                                      updateUser = {userService.updateUser}
+                                      userId = {userId}
+                                      user = {user}
+                                      />
                     </div>
 
 
@@ -114,7 +142,8 @@ const Profile = ({userid="superman", following="ab,cd,ef", followers="ab,cd,ef",
 
         </div>
     </div>
-    </BrowserRouter>
+    {/*</Route>*/}
+    </div>
     )
 }
 
