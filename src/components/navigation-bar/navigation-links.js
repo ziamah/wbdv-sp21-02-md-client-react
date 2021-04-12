6cb379396c5157e35bfe4ff62ba9e548c6e38927
mirandadayadkins <mirandaday16@gmail.com>
2021-04-12 13:12:58 -0400

    added logout to nav bar

--- src/components/navigation-bar/navigation-links.js
+++ src/components/navigation-bar/navigation-links.js
@@ -13,11 +13,15 @@ const NavigationLinks = () => {
             <Link to="/profile">
             <button className="btn wbdv-navbar-btn">profile</button>
             </Link>
+            <button className="btn wbdv-navbar-btn">
+                logout
+            </button>
             <Link to="/new-recipe">
             <button className="btn wbdv-navbar-btn">
                 <i className="fas fa-plus-circle"></i>
             </button>
             </Link>
+
         </>
     )
 }
