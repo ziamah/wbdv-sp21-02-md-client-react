195b7023c91c1e8abbc9b6199ec2180290c1c375
mirandadayadkins <mirandaday16@gmail.com>
2021-04-21 14:43:44 -0400

    fixed urls

--- src/components/profile-services/user-service.js
+++ src/components/profile-services/user-service.js
@@ -1,7 +1,7 @@
 //const TOPICS_URL = "http://localhost:8080/api/topics";
 //const WIDGETS_URL = "http://localhost:8080/api/widgets";
-// const USERS_URL = "http://localhost:8080/api/users";
-const USERS_URL = process.env.REACT_APP_USERS_URL;
+const USERS_URL = "http://localhost:8080/api/users";
+// const USERS_URL = process.env.REACT_APP_USERS_URL;
 
 
 export const findAllUsers = () =>
