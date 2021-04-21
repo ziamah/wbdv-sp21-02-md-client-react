8ae339b8f1161614357df7e6a1f0ef3bfab2ed15
mirandadayadkins <mirandaday16@gmail.com>
2021-04-21 14:37:48 -0400

    fixing urls for heroku

--- src/components/profile-services/user-service.js
+++ src/components/profile-services/user-service.js
@@ -1,6 +1,8 @@
 //const TOPICS_URL = "http://localhost:8080/api/topics";
 //const WIDGETS_URL = "http://localhost:8080/api/widgets";
-const USERS_URL = "http://localhost:8080/api/users";
+// const USERS_URL = "http://localhost:8080/api/users";
+const USERS_URL = process.env.REACT_APP_USERS_URL;
+
 
 export const findAllUsers = () =>
     fetch(USERS_URL)
