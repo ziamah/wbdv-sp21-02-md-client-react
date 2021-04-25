1d08bfea53b214ab1c3281e50436d1c72475469d
ziam <ziam@husky.neu.edu>
2021-04-25 14:12:15 -0400

    review service, can create and fetch created reviews

--- src/components/details-page/review-card.js
+++ src/components/details-page/review-card.js
@@ -1,4 +1,16 @@
-const ReviewCard = ({user}) => {
+import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import reviewService from "../../services/review-service";
+
+const ReviewCard = ({
+                        user,
+                        recipid,
+                        rating,
+                        reviewText,
+                        reviewer,
+    recipeName
+                    }) => {
+    const {id} = useParams()
     return (
         <div className="wbdv-review-card wbdv-widget-interior">
             <div className="row">
@@ -12,7 +24,7 @@ const ReviewCard = ({user}) => {
                     <div className="row">
                         <h3 className="h3 wbdv-padded-img">
                             {/*TODO: Fill in content programmatically*/}
-                            Review Title
+                            Review Title {recipeName}
                             <div className="wbdv-padded-icon float-right">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -25,13 +37,16 @@ const ReviewCard = ({user}) => {
                     {/*TODO: Link to profile page*/}
                     <div className="wbdv-link-text">
                         {/*    TODO: Fill username programmatically*/}
-                        username
+                        username: {reviewer}
                     </div>
                     <div className="row wbdv-widget-interior">
-                        Review text Camembert de normandie chalk and cheese fromage frais.
-                        Edam mozzarella cream cheese lancashire dolcelatte the big cheese gouda roquefort.
-                        Cheesy feet mascarpone dolcelatte fondue bavarian bergkase lancashire dolcelatte
-                        cheesecake.
+                        {reviewText}
+                        {/*Review text Camembert de normandie chalk and cheese fromage frais.*/}
+                        {/*Edam mozzarella cream cheese lancashire dolcelatte the big cheese gouda*/}
+                        {/*roquefort.*/}
+                        {/*Cheesy feet mascarpone dolcelatte fondue bavarian bergkase lancashire*/}
+                        {/*dolcelatte*/}
+                        {/*cheesecake.*/}
                     </div>
                 </div>
 
--- src/components/details-page/write-a-review-form.js
+++ src/components/details-page/write-a-review-form.js
@@ -1,6 +1,24 @@
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import React, {useState} from "react";
+import {createReview} from "../../services/review-service";
 
-const WriteAReview = () => {
+const WriteAReview = (user) => {
+    const {id} = useParams()
+    const [rating, setRating] = useState("1");
+    const [reviewText, setReviewText] = useState("");
+    const [reviewTitle, setReviewTitle] = useState("");
+
+    const handleSubmit = async () => {
+        const newReview = {
+            userID: user.userId,
+            recipeID: id,
+            Rating: rating,
+            reviewText: reviewText,
+            reviewTitle: reviewTitle
+        }
+        createReview(id, newReview)
+    }
+
     return (
         <div className="wbdv-widget-container-light">
             <div className="wbdv-contrast-header">
@@ -9,10 +27,13 @@ const WriteAReview = () => {
                 </h3>
             </div>
             <div className="wbdv-widget-interior">
-                <label htmlFor="star-rating-select"  className="wbdv-padded-icon">
+                <label htmlFor="star-rating-select" className="wbdv-padded-icon">
                     Choose a rating:
                 </label>
-                <select id="star-rating-select">
+                <select id="star-rating-select"
+                        onChange={(e) => setRating(e.target.value)}
+                        value={rating}
+                        className="form-control">
                     <option value="1">&#9733;</option>
                     <option value="2">&#9733; &#9733;</option>
                     <option value="3">&#9733; &#9733; &#9733;</option>
@@ -21,12 +42,18 @@ const WriteAReview = () => {
                 </select>
             </div>
             <div className="wbdv-widget-interior">
-                <textarea className="col-12" placeholder="Write your review here."></textarea>
+                <textarea className="col-12"
+                          placeholder="Write your review here."
+                          onChange={(event) => setReviewText(event.target.value)}
+                          value={reviewText}></textarea>
             </div>
             <div className="row wbdv-center-in-div">
                 {/*TODO: Enable button to post new review to the server*/}
                 <Link to={"#"}>
-                    <button className="btn wbdv-affirmative-btn">
+                    <button className="btn wbdv-affirmative-btn"
+                            onClick={async () => {
+                                await handleSubmit()
+                            }}>
                         POST
                     </button>
                 </Link>
